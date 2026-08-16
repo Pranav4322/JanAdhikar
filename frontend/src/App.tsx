ac47906049822e75bd67a75725aa6bee3754a3de
Pranav4322 <pranavmore925@gmail.com>
2026-08-17 01:36:04 +0530

    update app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { AuthProvider } from './lib/AuthContext';
 import Landing from './pages/Landing';
 import Auth from './pages/Auth';
 import ReportIssue from './pages/ReportIssue';
+import TrackComplaint from './pages/TrackComplaint';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/report" element={<ReportIssue />} />
+          <Route path="/track" element={<TrackComplaint />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
