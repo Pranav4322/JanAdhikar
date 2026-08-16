4f39ba5a7d410d2ab8d381bb670f876a6422965c
Pranav4322 <pranavmore925@gmail.com>
2026-08-17 01:30:03 +0530

    update app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './lib/AuthContext';
 import Landing from './pages/Landing';
 import Auth from './pages/Auth';
+import ReportIssue from './pages/ReportIssue';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/auth" element={<Auth />} />
+          <Route path="/report" element={<ReportIssue />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
